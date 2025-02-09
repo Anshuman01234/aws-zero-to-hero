ddf11e8b0cb8fb9924177696169b872642481d4a
Anshuman01234 <123363779+Anshuman01234@users.noreply.github.com>
2025-02-09 18:14:43 +0530

    deleted unused apps

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull iamanshu007/simple-pyth-flsk-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:8000 docker pull iamanshu007/simple-pyth-flsk-app
